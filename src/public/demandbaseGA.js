be2df214e9542d41ec2adcbbebc9f9c865642dda
mdowns@demandbase.com <mdowns@demandbase.com>
2013-09-24 10:35:06 -0700

    new compiled hosted files

--- src/public/demandbaseGA.js
+++ src/public/demandbaseGA.js
@@ -1,14 +1,178 @@
-"undefined"===typeof window.console&&(window.console={log:function(){},debug:function(){}});"undefined"===typeof window.localStorage&&(window.localStorage=function(){});"use strict";var Demandbase=window.Demandbase||{};Demandbase.utils=window.Demandbase.utils||{};
-Demandbase.utils={name:"Demandbase Utilities",_version:"1.0",logging:!0,djq:null,getQueryParam:function(a){for(var b={},c=window.location.search.substring(1).split("&"),d=0;d<c.length;d++){var e=c[d].split("=");b[e[0]]=e[1]}return b[a]},putLS:function(a){for(var b in a)localStorage.setItem(b,a[b]),this._log("Set localStorage field: "+b+" : "+a[b])},getLS:function(a){if(localStorage.getItem(a))return localStorage.getItem(a);this._log("Requested field: "+a+" was not found in localStorage.");return null},
-authorize:function(a){var b=jQuery.noConflict()||Demandbase.jQuery;b?b.ajax({url:"http://api.demandbase.com/api/v2/ip.json",data:{key:a},dataType:"jsonp",timeout:2E3,success:function(b,d,e){Demandbase.utils._log("Validating key: "+a+".... Confirmed authorized key")},error:function(b,d,e){Demandbase.utils._log("Validating key: "+a+".... is NOT a valid key")}}):Demandbase.utils._log("Could not authorize key...jQuery is missing.")},_log:function(a){"undefined"===typeof window.console||!this.logging&&
-"true"!==this.getQueryParam("db_logging")||window.console.log("Demandbase: "+a)},runConnectors:function(a){}};Demandbase.utils.runConnectors=function(a){};"undefined"===typeof window.console&&(window.console={log:function(){},debug:function(){}});"use strict";Demandbase=window.Demandbase||{};Demandbase.Connectors=window.Demandbase.Connectors||{};Demandbase.CompanyProfile=window.Demandbase.CompanyProfile||{};Demandbase.debug=!0;Demandbase.logging=!0;Demandbase.getQueryParam=function(a){for(var b={},c=window.location.search.substring(1).split("&"),d=0;d<c.length;d++){var e=c[d].split("=");b[e[0]]=e[1]}return b[a]};
-Demandbase._log=function(a){"undefined"!==typeof window.console||!Demandbase.logging&&"true"!==Demandbase._getQueryParam("db_logging")||window.console.log("DBContentConnector: "+a)};void 0===Demandbase.runConnectors&&(Demandbase.runConnectors=function(){});
-Demandbase.IP={_key:"YOUR_KEY_HERE",_useTestIp:!1,_testIpAddress:"3.0.0.1",_version:1,_parser:function(a){if(!a)return"";try{a=Demandbase.IP._flattenData(a),Demandbase.CompanyProfile=a,Demandbase.runConnectors(a)}catch(b){Demandbase.debug&&alert("DB IP Connector Error: "+b)}},_flattenData:function(a){for(var b in a)if("object"==typeof a[b]&&null!==a[b]){for(var c in a[b])a[b+"_"+c]=a[b][c];delete a[b]}return a},_load:function(){var a=document.createElement("script");a.async=!0;a.id="db_ip_api";a.src=
-("https:"==document.location.protocol?"https://":"http://")+"api.demandbase.com/api/v2/ip.json?key="+this._key+"&referrer="+document.referrer+"&page="+document.location.href+"&page_title="+document.title+"&callback=Demandbase.IP._parser&query";this._useTestIp&&(""==this._testIpAddress&&(this._testIpAddress=Demandbase._getQueryParam("db_ip")),""!==this._testIpAddress&&(a.src=a.src+"="+this._testIpAddress));document.getElementsByTagName("head")[0].appendChild(a)}};Demandbase.IP._load();
-Demandbase.runConnectors=function(a){Demandbase.Connectors.AudienceManager.send(Demandbase.CompanyProfile)};
-Demandbase.Connectors.AudienceManager={domain:"my_domain.n.net",dbToAmMap:{audience:"",industry:"",company_name:"",revenue_range:""},send:function(a){var b=document.createElement("script");b.async=!0;b.id="adbe_am_api";var c="";for(field in this.dbToAmMap){var d=Demandbase.CompanyProfile[field]||"default";d.replace(" ","+");c=c+"c_"+this.dbToAmMap[field]+"="+d+"&"}b.src="http://"+this.domain+"/event?d_stuff=1&d_dst=1&d_rtbd=json&d_ld=some+data+to+log&"+c+"d_cb=Demandbase.Connectors.AudienceManager.callback";
-document.getElementsByTagName("head")[0].appendChild(b);alert("sending to ADBE AM for "+a.company_name||"non-company visitor")},callback:function(a){alert(a)}};Demandbase=window.Demandbase||{};Demandbase.Connectors=window.Demandbase.Connectors||{};
-Demandbase.Connectors.Google_Analytics={name:"Demandbase Google Analytics Connector",key:"",fields:["audience","industry","revenue_range","employee_range","company_name"],version:"4.0",dbDataSet:null,track:function(a){try{var b=Demandbase.Connectors.Google_Analytics;a=b._flatA(a);b.dbDataSet=a;for(var c in b.fields){var d=b.fields[c],e=a[b.fields[c]]||"(Non-company Visitor)";b._var(parseInt(c)+1,d,e,1);b._logE(parseInt(c)+1+" "+d+" : "+e)}if(a){var f=a.audience||"(Non-company Visitor)",g=a.audience_segment||
-"(Non-company Visitor)",d=a.company_name||"(Non-company Visitor)";b._cEvent(f,g,d)}b._event()}catch(h){Demandbase.Connectors.Google_Analytics._logE("Integration Error: "+h)}},load:function(){try{window._gaq||(window._gaq=[]);var a=document.createElement("script");a.type="text/javascript";a.async=!0;a.src=("https:"==document.location.protocol?"https://":"http://")+"api.demandbase.com/api/v2/ip.json?key="+this.key+"&callback=Demandbase.Connectors.Google_Analytics.track&page="+document.URL+"&referrer="+
-document.referrer+"&page_title="+document.title;var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b);_gaq.push(["_addDevId","NE7T9"]);Demandbase.Connectors.Google_Analytics._logE("Loaded Script "+a.src)}catch(c){this._logE("Script Error: "+c)}},_flatA:function(a){for(var b in a)if("object"==typeof a[b]&&null!==a[b]){for(var c in a[b])a[b+"_"+c]=a[b][c];delete a[b]}return a},_logE:function(a){"undefined"!==window.console&&"undefined"!==typeof console&&console.log("DB GA: "+
-a)},_p:function(a,b,c,d,e,f){window._gaq.push([a,b,c,d,e,f])},_var:function(a,b,c,d){this._p("_setCustomVar",a,b,c,d)},_event:function(){this._p("_trackEvent","Demandbase","API Resolution","IP API",0,1)},_cEvent:function(a,b,c){this._p("_trackEvent",a,b,c,0,1);this._logE("Custom Event Tracked: "+a+" : "+b+" : "+c)}};Demandbase.Connectors.Google_Analytics.load();
+if (typeof window.console === "undefined") window.console = {
+    log: function () {},
+    debug: function () {}
+};
+if (typeof window.localStorage === "undefined") window.localStorage = function () {};
+var Demandbase = window.Demandbase || {};
+Demandbase.utils = window.Demandbase.utils || {};
+Demandbase.utils = {
+    name: "Demandbase Utilities",
+    _version: "1.0",
+    logging: true,
+    djq: null,
+    getQueryParam: function (param) {
+        var params = {};
+        var qs = window.location.search.substring(1);
+        var pairs = qs.split("&");
+        for (var i = 0; i < pairs.length; i++) {
+            var nvArray = pairs[i].split("=");
+            var name = nvArray[0];
+            var value = nvArray[1];
+            params[name] = value
+        }
+        return params[param]
+    },
+    flattenData: function (data) {
+        for (var d in data) if (typeof data[d] == "object" && data[d] !== null) {
+            for (var nd in data[d]) data[d + "_" + nd] = data[d][nd];
+            delete data[d]
+        }
+        return data
+    },
+    putLS: function (data) {
+        for (var item in data) {
+            localStorage.setItem(item, data[item]);
+            this._log("Set localStorage field: " + item + " : " + data[item])
+        }
+    },
+    getLS: function (value) {
+        if (localStorage.getItem(value)) return localStorage.getItem(value);
+        else {
+            this._log("Requested field: " + value + " was not found in localStorage.");
+            return null
+        }
+    },
+    authorize: function (testKey) {
+        var djq = jQuery.noConflict() || Demandbase.jQuery;
+        if (djq) djq.ajax({
+            url: "http://api.demandbase.com/api/v2/ip.json",
+            data: {
+                key: testKey
+            },
+            dataType: "jsonp",
+            timeout: 2E3,
+            success: function (d, t, x) {
+                Demandbase.utils._log("Validating key: " + testKey + ".... Confirmed authorized key")
+            },
+            error: function (d, t, x) {
+                Demandbase.utils._log("Validating key: " + testKey + ".... is NOT a valid key")
+            }
+        });
+        else Demandbase.utils._log("Could not authorize key...jQuery is missing.")
+    },
+    _log: function (msg) {
+        if (typeof window.console !== "undefined" && (this.logging || this.getQueryParam("db_logging") === "true")) window.console.log("Demandbase: " + msg)
+    },
+    runConnectors: function (data) {
+        alert("Testing function extension");
+        this._log("Testing function extension")
+    }
+};
+Demandbase.utils.runConnectors = function (data) {};
+var Demandbase = window.Demandbase || {};
+Demandbase.utils = window.Demandbase.utils || {};
+Demandbase.Connectors = window.Demandbase.Connectors || {};
+Demandbase.IP = {
+    name: "Demandbase IP API Wrapper",
+    _version: 1,
+    CompanyProfile: {},
+    _key: window._dbc.key || "",
+    _useTestIp: false,
+    _testIpAddress: window._dbc.testIp || "3.0.0.1",
+    _debug: true,
+    _parser: function (data) {
+        if (!data) return "";
+        try {
+            var self = Demandbase.IP,
+                dbu = Demandbase.utils;
+            data = dbu.flattenData(data);
+            this.CompanyProfile = data;
+            dbu.runConnectors(data)
+        } catch (e) {
+            if (Demandbase.debug) alert("DB IP Connector Error: " + e)
+        }
+    },
+    _load: function () {
+        var s = document.createElement("script");
+        s.async = true;
+        s.id = "db_ip_api";
+        s.src = ("https:" == document.location.protocol ? "https://" : "http://") + "api.demandbase.com/api/v2/ip.json?key=" + this._key + "&referrer=" + document.referrer + "&page=" + document.location.href + "&page_title=" + document.title + "&callback=Demandbase.IP._parser&query";
+        if (this._useTestIp) {
+            if (this._testIpAddress == "") {
+                this._testIpAddress = Demandbase._getQueryParam("db_ip");
+                Demandbase.utils._log("Query IP API...overriding query parameter from URL: " + this._testIpAddress)
+            }
+            if (this._testIpAddress !== "") {
+                s.src = s.src + "=" + this._testIpAddress;
+                Demandbase.utils._log("Query IP API...overriding query Demandbase.IP: " + this._testIpAddress)
+            }
+        }
+        document.getElementsByTagName("head")[0].appendChild(s);
+        Demandbase.utils._log("Calling IP API...")
+    }
+};
+Demandbase.IP._load();
+Demandbase = window.Demandbase || {};
+Demandbase.Connectors = window.Demandbase.Connectors || {};
+Demandbase.Connectors.Google_Analytics = {
+    name: "Demandbase Google Analytics Connector",
+    key: "",
+    fields: window._dbc.gaCustomVars || [],
+    version: "4.1",
+    dbDataSet: null,
+    track: function (data) {
+        try {
+            var dbgac = Demandbase.Connectors.Google_Analytics,
+                dflt = "(Non-company Visitor)";
+            dbgac.dbDataSet = data;
+            for (var field in dbgac.fields) {
+                var lbl = dbgac.fields[field];
+                var val = data[dbgac.fields[field]] || dflt;
+                dbgac._var(parseInt(field) + 1, lbl, val, 1);
+                Demandbase.utils._log(parseInt(field) + 1 + " " + lbl + " : " + val)
+            }
+            if (data) {
+                var cat = data["audience"] || dflt;
+                var act = data["audience_segment"] || dflt;
+                var lbl = data["company_name"] || dflt;
+                dbgac._cEvent(cat, act, lbl)
+            }
+            dbgac._event()
+        } catch (e) {
+            Demandbase.utils._log("Integration Error: " + e)
+        }
+    },
+    load: function () {
+        try {
+            if (!window._gaq) window._gaq = [];
+            if (!Demandbase.IP.CompanyProfile) alert("Demandbase.IP.CompanyProfile: " + Demandbase.IP.CompanyProfile);
+            this.track(Demandbase.IP.CompanyProfile);
+            _gaq.push(["_addDevId", "NE7T9"])
+        } catch (e) {
+            Demandbase.utils._log("Script Error: " + e)
+        }
+    },
+    _p: function (t, v1, v2, v3, v4, v5) {
+        window._gaq.push([t,
+        v1, v2, v3, v4, v5])
+    },
+    _var: function (i, k, v, s) {
+        this._p("_setCustomVar", i, k, v, s)
+    },
+    _event: function () {
+        this._p("_trackEvent", "Demandbase", "API Resolution", "IP API", 0, 1)
+    },
+    _cEvent: function (cat, act, lbl) {
+        this._p("_trackEvent", cat, act, lbl, 0, 1);
+        Demandbase.utils._log("Custom Event Tracked: " + cat + " : " + act + " : " + lbl)
+    }
+};
+(function () {
+    var runner = Demandbase.utils.runConnectors;
+    Demandbase.utils.runConnectors = extRunConnectors;
+
+    function extRunConnectors() {
+        runner.apply(Demandbase.utils);
+        Demandbase.Connectors.Google_Analytics.load()
+    }
+})();
