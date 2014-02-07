5b3dbbeb0cea26add705f4334384351d91237822
mdowns@demandbase.com <mdowns@demandbase.com>
2014-02-06 19:01:03 -0800

    renamed form connector and drupal module folders, added read me and template implementation to form connector and moved old stuff into legacy folder

---
+++ FormConnector/README.md
@@ -0,0 +1,6 @@
+#Form Connector
+The template provided here is a basic starting point for you Form Connector implementation.
+
+**Note:** In most cases, your Demandbase Solutions Consultant will provide the initial configuration snippet to you based on your specified requirements and test form.
+
+For the soup-to-nuts configuration and installation instructions, visit our [Getting Started Guide](https://gist.github.com/demandbaselabs/8677440)
---
+++ FormConnector/configTemplate.js
@@ -0,0 +1,51 @@
+//Start Demandbase Form Connector Implementation
+window.dbAsyncInit = function() {
+    //Form Connector Configuration
+    var dbf = Demandbase.Connectors.WebForm;
+    dbf.connect({
+        emailID: "ID_OF_EMAIL_FIELD",   //must be ID
+        companyID: "ID_OF_COMPANY_FIELD", //must be ID
+        key: 'YOUR_KEY_HERE',
+        toggleFieldList: ['fieldIDsGoHere'],
+        /* These can be name or ID */
+        fieldMap: {
+            'company_name': '',
+            'industry': '',
+            'sub_industry': '',
+            'primary_sic': '',
+            'revenue_range': '',
+            'annual_sales': '',
+            'employee_range': '',
+            'employee_count': '',
+            'street_address': '',
+            'city': 'city',
+            'state': '',
+            'zip': '',
+            'country': '',
+            'country_name': '',
+            'latitude': '',
+            'longitude': '',
+            'phone': '',
+            'web_site': '',
+            'stock_ticker': '',
+            'traffic': '',
+            'b2b': '',
+            'b2c': '',
+            'fortune_1000': '',
+            'forbes_2000': '',
+            'duns_num': '',
+            'demandbase_sid': '',
+            'data_source': '',
+            'audience': '',
+            'audience_segment': ''
+        }
+    });
+};
+
+(function() {
+    //Retrieve Form Connector core file from the cloud
+    var dbt = document.createElement('script'); dbt.type = 'text/javascript'; dbt.async = true; dbt.id = 'demandbase-form';
+    dbt.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'scripts.demandbase.com/formWidget.js'; //'formWidgetDebug.js'; //
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(dbt, s);
+})();
+//End Demandbase Form Connector Implementation
