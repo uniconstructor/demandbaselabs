316bce8d199efbbee0d2de969065f7d143fd2e15
Matthew Downs <downs.matt@gmail.com>
2014-02-12 16:09:53 -0800

    starter pack usage instruction, added switch in callback

--- JavaScriptSDK/README.md
+++ JavaScriptSDK/README.md
@@ -1,2 +1,17 @@
 #Demandbase JavaScript SDK
-Visit the [Getting Started Guide](https://gist.github.com/demandbaselabs/8365109) for details.
+
+##Starter Pack
+The JS file provided here is pre-configured with 5 commonly-used, broad-based visitor segments.
+
+Start personalizing and targeting your site content by simply adding `sdkStarterPack.js` to your HTML, then add your Demandbase Content Module key.
+
+
+##Getting Started Guide
+The sample implementation provided here is just a starting point.  This solution is completely configurable and can match your target segments.
+
+Visit the [Getting Started Guide](https://gist.github.com/demandbaselabs/8365109) for detailed configuration and usage instructions.
+
+Talk to your Demandbase Solutions Consultant for details on configuring the SDK.
+
+
+
--- JavaScriptSDK/sdkStarterPack.js
+++ JavaScriptSDK/sdkStarterPack.js
@@ -8,16 +8,31 @@
         ,{ name: 'Audience_Edu', attribute: 'audience', operator: '=', value: 'Education' }
         ,{ name: 'Audience_Govt', attribute: 'audience', operator: '=', value: 'Government' }
     ];
-    __db.segments = [
-    ];
+    __db.segments = [];
     __db.callback = function(company) {
         /**
           Content customization code goes here!
           Add any function calls or other code here to be run after the Demandbase Company Profile is available
           'company' argument is equivalent to Demandbase.Segments.CompanyProfile
         **/
-    };
 
+        //Add Customizations in each section below
+        if(Demandbase.Segments.Audience_SMB) {
+            //Small business
+        } else if(Demandbase.Segments.Audience_MidMkt) {
+            //Mid-Market
+        } else if(Demandbase.Segments.Audience_Ent) {
+            //Enterprise
+        } else if(Demandbase.Segments.Audience_Edu) {
+            //Education
+        } else if(Demandbase.Segments.Audience_Govt) {
+            //Government
+        } else {
+            // Default
+            //Demandbase Recommends - Always define default experience here...
+        }
+
+    };
     (function() {
         var db = document.createElement('script'); db.type = 'text/javascript'; db.async = true;
         db.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'scripts.demandbase.com/demandbase-sdk.js';
