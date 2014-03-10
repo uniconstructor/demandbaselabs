8bc62c64144481a09afc37f0bb5ba4e97e87a8dc
Matthew Downs <downs.matt@gmail.com>
2014-03-10 14:54:19 -0700

    changed entitlements to company profile

--- WordPress_Plugin/demandbase_entitlements_page.php
+++ WordPress_Plugin/demandbase_entitlements_page.php
@@ -1,17 +1,20 @@
-<div class="wrap">  
-    <?php    echo "<h2>" . __( 'Your Demandbase Entitlements', 'demandbase_translation' ) . "</h2>"; ?>  
-      
-    <h3>About Entitlements</h3>
-    
-    <p>Entitlements are part of your contract with Demandbase, Inc. They can <strong>ONLY</strong> be changed by contacting your representative.</p>
-    
-	<h3>Your Entitlements</h3>
-	
-	<?php
-		$entitlements = demandbase_get_response(TRUE, TRUE);
-		
-		foreach ($entitlements as $entitlement_k => $entitlement_v) {
-			echo '<code>'.$entitlement_k.'</code><br />';
-		}
-	?>
+<div class="wrap">
+    <?php    echo "<h2>" . __( 'Demandbase Company Profile Attributes', 'demandbase_translation' ) . "</h2>"; ?>
+
+    <h3>About Company Profile Attributes</h3>
+
+    <p>The Company Profile attributes provided by Demandbase are configured for your Demandbase Content Module key.  To add or remove attributes, contact your Customer Success Manager or Demandbase Support. </p>
+    <p>If you have setup Account Watch, your custom fields will show here.</p>
+
+    <h3>Available Attributes</h3>
+
+    <?php
+        $entitlements = demandbase_get_response(TRUE, TRUE);
+        if(!is_null($entitlements)) {
+            foreach ($entitlements as $entitlement_k => $entitlement_v) {
+                echo '<code>'.$entitlement_k.'</code><br />';
+            }
+        }
+
+    ?>
 </div>
